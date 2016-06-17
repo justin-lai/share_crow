2b895b35192b4ffa41afd8764f110ecdb67db4e0
Justin Lai <justin.th.lai@gmail.com>
2016-06-17 16:09:03 -0700

    more setup configuration of the front-end routing

--- client/index.js
+++ client/index.js
@@ -26,23 +26,3 @@ render(
   </Provider>),
   document.getElementById('app')
 );
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import shareApp from './reducers'
-import App from './components/App'
-
-// let store = createStore(shareApp);
-
-// render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('app')
-// )
-
-render(
-  <App />,
-  document.getElementById('app')
-)
