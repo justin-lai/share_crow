a8b0116ab93966f2db2fa7dd8289a557472dceac
Justin Lai <justin.th.lai@gmail.com>
2016-06-17 20:08:57 -0700

    completed user reducers and actions with prop connection to components

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { getUser, postUser, putUser, deleteUser } from '../actions/userActions.js';
 
-const App = () =>
-  <div>
-    Hello
-  </div>
-;
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default App;
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        Yo!
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  user: PropTypes.object.isRequired,
+  methods: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  const { user } = state;
+
+  return {
+    user,
+  };
+}
+
+const mapDispatchToProps = function mapDispatchToProps(dispatch) {
+  return {
+    methods: {
+      getUser: (id) => {
+        dispatch(getUser(id));
+      },
+      postUser: (data) => {
+        dispatch(postUser(data));
+      },
+      putUser: (data) => {
+        dispatch(putUser(data));
+      },
+      deleteUser: (data) => {
+        dispatch(deleteUser(data));
+      },
+    },
+  };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/reducers/reducers.js
+++ client/reducers/reducers.js
@@ -1,32 +1,19 @@
 import {
-  GET_USER, POST_USER, PUT_USER, DELETE_USER,
-  GET_ITEM, POST_ITEM, PUT_ITEM, DELETE_ITEM }
-from '../actions/actions';
+  USER_GET_RESPONSE,
+  USER_POST_RESPONSE,
+  USER_PUT_RESPONSE,
+  USER_DELETE_RESPONSE,
+} from '../actions/userActions';
 
 export const user = (state = {}, action) => {
   switch (action.type) {
-    case GET_USER:
+    case USER_GET_RESPONSE:
       return action.data;
-    case POST_USER:
+    case USER_POST_RESPONSE:
       return action.data;
-    case PUT_USER:
+    case USER_PUT_RESPONSE:
       return action.data;
-    case DELETE_USER:
-      return action.data;
-    default:
-      return state;
-  }
-};
-
-export const item = (state = {}, action) => {
-  switch (action.type) {
-    case GET_ITEM:
-      return action.data;
-    case POST_ITEM:
-      return action.data;
-    case PUT_ITEM:
-      return action.data;
-    case DELETE_ITEM:
+    case USER_DELETE_RESPONSE:
       return action.data;
     default:
       return state;
