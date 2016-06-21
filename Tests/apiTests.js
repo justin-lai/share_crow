a0b1ddf9cdfb004505b69c4d96ed636cea3e2914
Ben <b_chou@u.pacific.edu>
2016-06-20 17:13:44 -0700

    changed route base

--- Tests/apiTests.js
+++ Tests/apiTests.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 test('Google Maps Matrix Calcuations', (assert) => {
   // eslint-disable-next-line
   // tests with lat long of Danville, CA to Stockton CA sent though the server
-  fetch('http://localhost:3000/distanceMatrix?lat1=37.821593&long1=-121.999961&lat2=37.957702&long2=-121.290780')
+  fetch('http://localhost:3000/api/distanceMatrix?lat1=37.821593&long1=-121.999961&lat2=37.957702&long2=-121.290780')
   .then((res) => res.json())
   .then((responseData) => assert.equal(responseData.miles, '57.8 mi',
  'Google Maps Calculated Distance and ETA Correctly'));
