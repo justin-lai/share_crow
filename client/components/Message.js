33fcbc28f97e81f0e63bd198c0af2401555c58e0
Justin Lai <justin.th.lai@gmail.com>
2016-06-20 20:43:52 -0700

    styled message inbox

--- client/components/Message.js
+++ client/components/Message.js
@@ -1,12 +1,33 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
-const Message = ({ message }) => (
-  <div className="message">
-    <p className="subject"><strong>{message.subject}</strong></p>
-    <p className="sender">from <a href="/#/profile">{message.sender}</a> sent DD/MM/YYYY</p>
-    <p className="message-text">{message.text}</p>
-  </div>
-);
+class Message extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showText: false,
+    };
+
+    this.toggleText = this.toggleText.bind(this);
+  }
+
+  toggleText() {
+    this.setState({
+      showText: !this.state.showText,
+    });
+  }
+
+  render() {
+    const message = this.props.message;
+    return (
+      <div className="message" onClick={this.toggleText}>
+        <p className="subject">{message.subject}</p>
+        <p className="sender">from <a href="/#/profile">{message.sender}</a> sent DD/MM/YYYY</p>
+        {this.state.showText ? <p className="message-text">{message.text}</p> : null}
+      </div>
+    );
+  }
+}
 
 Message.propTypes = {
   message: PropTypes.object.isRequired,
--- client/components/MessageInbox.js
+++ client/components/MessageInbox.js
@@ -1,13 +1,24 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import Message from './Message.js';
 
-const MessageInbox = ({ messages }) => (
-  <div className="inbox">
-    {
-      messages.map(message => <Message message={message} />)
-    }
-  </div>
-);
+class MessageInbox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: 'inbox',
+    };
+  }
+
+  render() {
+    return (
+      <div id="inbox">
+        <div id="inbox-header">MESSAGES</div>
+        {this.props.messages.map(message => <Message message={message} />)}
+      </div>
+    );
+  }
+}
 
 MessageInbox.propTypes = {
   messages: PropTypes.array.isRequired,
