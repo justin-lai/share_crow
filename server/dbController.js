46c4f4724faaf2aa925392aae0c7f1126bf1d127
Ben <b_chou@u.pacific.edu>
2016-06-22 11:50:51 -0700

    all routes updates

--- server/dbController.js
+++ server/dbController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // const fetch = require('node-fetch');
 const bcrypt = require('bcrypt');
 const path = require('path');
@@ -5,15 +6,13 @@ const db = require(path.resolve(__dirname, '../../db/dbDesign.js'));
 
 module.exports = {
 
-  // SIGN UP FUNCTIONS
+  // ////////////////////////// SIGN UP FUNCTIONS ////////////////////////////
   // expects username, password, email, phoneNumber, address, aboutMe
   signup: (req, res) => {
-    // eslint-disable-next-line no-console
     console.log('POST //// SIGNUP ROUTE');
     // eslint-disable-next-line
     if (req.body.username && req.body.password && req.body.email && req.body.phoneNumber && req.body.address && req.body.aboutMe) {
       // generate a new hash based on password and make new entry in table
-      console.log(req.body);
       const hash = bcrypt.hashSync(req.body.password, 10);
       db.User.create({
         username: req.body.username,
@@ -28,13 +27,12 @@ module.exports = {
     }
   },
 
-  // LOGIN FUNCTIONS
+  // //////////////////////////// LOGIN FUNCTIONS ////////////////////////////
   // expects username, password
   login: (req, res) => {
     if (!req.query.password || !req.query.username) {
       res.status(400).send({});
     }
-    // eslint-disable-next-line no-console
     console.log('GET //// LOGIN ROUTE');
     db.User.findAll({
       where: {
@@ -54,11 +52,10 @@ module.exports = {
       .catch(err => err);
   },
 
-  // PROFILE FUNCTIONS
+  // //////////////////////////// PROFILE FUNCTIONS ////////////////////////////
   // expects id
   getUser: (req, res) => {
     // query database for a specific profile
-    // eslint-disable-next-line no-console
     console.log('GET //// getUser Route');
     if (req.query.id) {
       db.User.findAll({
@@ -78,19 +75,60 @@ module.exports = {
     }
   },
 
-  // expects username and id with an arbitrary number of parameters to change
+  // expects id with an arbitrary number of parameters to change
   updateProfile: (req, res) => {
-    // change database entry depending on parameters
-    // eslint-disable-next-line no-console
     console.log('PUT //// updateProfile Route');
-    res.sendStatus(200);
+
+    // change database entry depending on parameters
+    if (!req.body.id) {
+      res.status(400).send({});
+    } else {
+      const updateProfile = {
+        password: req.body.password || null,
+        email: req.body.email || null,
+        address: req.body.address || null,
+        phone: req.body.phoneNumber || null,
+        about: req.body.aboutMe || null,
+      };
+
+      if (!req.body.password) {
+        delete updateProfile.password;
+      } else {
+        updateProfile.password = bcrypt.hashSync(req.body.password, 10);
+      }
+      if (!req.body.email) {
+        delete updateProfile.email;
+      }
+      if (!req.body.address) {
+        delete updateProfile.address;
+      }
+      if (!req.body.phoneNumber) {
+        delete updateProfile.phone;
+      }
+      if (!req.body.aboutMe) {
+        delete updateProfile.about;
+      }
+      db.User.find(
+        {
+          where: {
+            id: req.body.id,
+          },
+        })
+        .then(queryData => queryData.updateAttributes(updateProfile))
+        .then(() => {
+          db.User.find({
+            where: {
+              id: req.body.id,
+            },
+          }).then(newEntry => res.status(200).send(newEntry));
+        });
+    }
   },
 
-  // PRIVATE MESSAGING FUNCTIONS
+  // //////////////////////////// PRIVATE MESSAGING FUNCTIONS ////////////////////////////
   // expects username and id
   getMessages: (req, res) => {
     // pulls all messages associated with username and id
-    // eslint-disable-next-line no-console
     console.log('GET //// getMessages');
     db.Messages.findAll({
       where: {
@@ -109,7 +147,6 @@ module.exports = {
   // expects sender_id, recipient_id, text
   postMessages: (req, res) => {
     // adds a new message entry in database
-    // eslint-disable-next-line no-console
     console.log('POST //// postMessages');
     if (req.body.text) {
       db.Messages.create({
@@ -123,7 +160,7 @@ module.exports = {
     }
   },
 
-  // RENTAL LISTING FUNCTIONS
+  // //////////////////////////// RENTAL LISTING FUNCTIONS ////////////////////////////
   // expects none or 1 filter parameter
   getListings: (req, res) => {
     console.log('GET //// getListings route');
@@ -157,10 +194,10 @@ module.exports = {
       where: searchFilters,
     }).then((items) => res.status(200).send(items));
   },
+
   // expects item, owner_id, max_fee, rental_fee, rental_period, image
   createListing: (req, res) => {
     // adds a new listing entry in database
-    // eslint-disable-next-line no-console
     console.log('POST //// createListing route');
     db.Listings.create({
       name: req.body.item,
@@ -174,27 +211,85 @@ module.exports = {
     res.status(201).send(req.body);
   },
 
-  // expects listing id and an arbitrary number of parameters
+  // expects listingId and an arbitrary number of parameters
   changeListing: (req, res) => {
     // modifies entry with 'listing id' in database
-    // eslint-disable-next-line no-console
-    console.log('changeListing route', req.body);
-    res.sendStatus(200);
+    console.log('PUT //// changeListing Route');
+
+    // change database entry depending on parameters
+    if (!req.body.listingId) {
+      res.status(400).send({});
+    } else {
+      const updateListing = {
+        renterId: req.body.renderId || null,
+        maxFee: req.body.maxFee || null,
+        rentalFee: req.body.rentalFee || null,
+        rentalPeriod: req.body.rentalPeriod || null,
+        itemImage: req.body.itemImage || null,
+      };
+
+      if (!req.body.renterId) {
+        delete updateListing.renterId;
+      }
+      if (!req.body.maxFee) {
+        delete updateListing.maxFee;
+      }
+      if (!req.body.rentalFee) {
+        delete updateListing.rentalFee;
+      }
+      if (!req.body.rentalPeriod) {
+        delete updateListing.rentalPeriod;
+      }
+      if (!req.body.itemImage) {
+        delete updateListing.itemImage;
+      }
+      db.Listings.find(
+        {
+          where: {
+            id: req.body.listingId,
+          },
+        })
+        .then(queryData => queryData.updateAttributes(updateListing))
+        .then(() => {
+          db.Listings.find({
+            where: {
+              id: req.body.listingId,
+            },
+          }).then(newEntry => res.status(200).send(newEntry));
+        });
+    }
   },
 
   // expects listing id
   returnedListing: (req, res) => {
     // call change listing to change renting period to 'complete'
-    // eslint-disable-next-line no-console
-    console.log('returnListing route', req.body);
-    res.sendStatus(410);
+    console.log('DELETE //// returnedListing route');
+    if (!req.body.listingId) {
+      res.status(400).send({});
+    } else {
+      db.Listings.find(
+        {
+          where: {
+            id: req.body.listingId,
+          },
+        })
+        .then(queryData => queryData.updateAttributes({
+          itemReturned: true,
+        }))
+        .then(() => {
+          db.Listings.find({
+            where: {
+              id: req.body.listingId,
+            },
+          }).then(newEntry => res.status(200).send(newEntry));
+        });
+    }
   },
 
-  // USER REVIEW FUNCTIONS
+  // //////////////////////////// USER REVIEW FUNCTIONS ////////////////////////////
   // expects lenderId
   getUserReviews: (req, res) => {
     // returns all entries associated with username and id from database
-    // eslint-disable-next-line no-console
     console.log('GET //// getUserReviews route');
 
     db.Reviews.findAll({
@@ -214,23 +309,24 @@ module.exports = {
   // expects reviewerId, lenderId, rating, text
   createUserReview: (req, res) => {
     // add a new review entry in database
-    // eslint-disable-next-line no-console
     console.log('POST //// createUserReview route');
-
-    db.Reviews.create({
-      lenderId: req.body.lenderId,
-      reviewerId: req.body.reviewerId,
-      rating: req.body.rating,
-      text: req.body.text,
-    })
-    .then(queryData => res.status(200).send(queryData));
+    if (!req.body.lenderId || !req.body.reviewerId || !req.body.rating || !req.body.text) {
+      res.status(400).send({});
+    } else {
+      db.Reviews.create({
+        lenderId: req.body.lenderId,
+        reviewerId: req.body.reviewerId,
+        rating: req.body.rating,
+        text: req.body.text,
+      })
+      .then(queryData => res.status(200).send(queryData));
+    }
   },
 
   // expects reviewId
   deleteUserReview: (req, res) => {
     // id associated with the review must match the user id
     // deletes a user review from database by id
-    // eslint-disable-next-line no-console
     console.log('DELETE //// deleteUserReview route');
     db.Reviews.destroy({
       where: {
