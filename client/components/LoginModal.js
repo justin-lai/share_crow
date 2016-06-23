bf04e00234bd2bf9440e3aac0761086ec72a2d5c
Cathy <caathylee@gmail.com>
2016-06-22 20:33:24 -0700

    Set up login modal component

---
+++ client/components/LoginModal.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+
+class LoginModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() { this.setState({ open: true }); }
+  closeModal() { this.setState({ open: false }); }
+
+  render() {
+    return (
+      <div className="login-wrapper">
+        <div
+          className="login-modal"
+          onClick={this.openModal}
+        ><span className="glyphicon glyphicon-log-in"></span>Login</div>
+        <Modal isOpen={this.state.open}>
+          <h1>Login</h1>
+          <button onClick={this.closeModal}>Close</button>
+          <p><input placeholder="Username or Email" /></p>
+          <p><input placeholder="Password" /></p>
+          <input type="submit" value="Login" />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default LoginModal;
