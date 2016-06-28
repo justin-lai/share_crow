3076c6cac4f0176429485e48ecaa8886c1dd1231
Arthur Livingston <arthurlivingston@Arthurs-MacBook-Air.local>
2016-06-27 18:11:01 -0700

    Images are being updated in S3 but files are currently corrupted

--- db/s3bucket.js
+++ db/s3bucket.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 const config = require('../config.js');
-const image = fs.readFileSync('./photos/Char-Broil-Four-burner-Kettle-Grill-P15074099.jpg');
+const image = fs.readFileSync('./photos/smart_pig.jpg');
 
 AWS.config.update({
   accessKeyId: config.AWSAccessKeyId,
@@ -13,12 +13,11 @@ const s3 = new AWS.S3();
 s3.createBucket({ Bucket: 'sharecrowimages' }, () => {
   const params = {
     Bucket: 'sharecrow',
-    Key: 'grill2.jpg',
+    Key: 'smart_pig.jpg',
     ACL: 'public-read',
     Body: image,
     ContentType: 'image/jpeg',
   };
-
   s3.putObject(params, (err, data) => {
     if (err) {
       //eslint-disable-next-line
