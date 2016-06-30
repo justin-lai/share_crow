b311f34e67ab31d6b7284363624e1ad5c183e311
justin-lai <justin.th.lai@gmail.com>
2016-06-30 11:17:44 -0700

    fix to postAnItemModal

--- client/components/PostAnItemModal.js
+++ client/components/PostAnItemModal.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { getCategory } from '../actions/categoryActions.js';
 import Modal from 'react-modal';
-import ImportImage from './importImage';
+import ImportImage from './ImportImage';
 
 
 class PostAnItemModal extends Component {
