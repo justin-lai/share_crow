ec945d2339eba388b192c1be9c5af3d4bca8cd93
Arthur Livingston <arthurlivingston@Arthurs-MacBook-Air.local>
2016-07-01 16:48:08 -0700

    additional work complete on payment

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,4 @@ module.exports = {
     "plugins": [
         "react"
     ]
-<<<<<<< 187edce3f988865473f12efc3c1ae8edfc47bc4a
 };
-=======
-};
->>>>>>> initiated styleguide linting and refactored actions and reducers
