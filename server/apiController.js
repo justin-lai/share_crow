a5e72e5e0501b019e0275f5f3fd2284d7d2287b5
Ben <b_chou@u.pacific.edu>
2016-07-02 12:17:25 -0700

    text coordination of phone numbers

--- server/apiController.js
+++ server/apiController.js
@@ -6,6 +6,7 @@ const baseLink = 'https://maps.googleapis.com/maps/api/distancematrix/json?units
 const fetch = require('node-fetch');
 const path = require('path');
 const db = require(path.resolve(__dirname, '../../db/dbDesign.js'));
+const Sequelize = require('sequelize');
 
 module.exports = {
   // find the distance between two points, given 2 lat,long pairs
@@ -29,30 +30,77 @@ module.exports = {
   },
 
   sendTextNotification: (req, res) => {
-    console.log(req.body);
-    db.User.find({
-      where: {
-        id: req.body.recipientId,
-      },
-    }).then(response => {
-      // eslint-disable-next-line
-      const recipientPhoneNumber = response.dataValues.phone.replace(/[-\(\)]/g, '');
-      console.log(recipientPhoneNumber, '######');
-      client.sendSms({
-        to: `+1${recipientPhoneNumber}`,
-        from: '+19259058241',
-        body: `${req.body.text}\nPlease check your notifications at www.sharecrow.com to respond.`,
-      }, (error, receiptMessage) => {
-        if (!error) {
-          // eslint-disable-next-line no-console
-          console.log('Message sent on: ${receiptMessage.dateCreated}');
-        } else {
-          // eslint-disable-next-line no-console
-          console.log('Oops! There was an error.');
-        }
+    if (req.body.sendPhoneNumbers) {
+      db.Listings.find({
+        where: {
+          id: req.body.listingId,
+        },
+      }).then(response => response.dataValues.name)
+          .then(itemName => {
+            db.User.findAll({
+              where: Sequelize.or({
+                id: req.body.recipientId,
+              }, {
+                id: req.body.senderId,
+              }),
+            }).then(response => {
+              const phone1 = response[0].dataValues.phone.replace(/[-\(\)]/g, '');
+              const phone2 = response[1].dataValues.phone.replace(/[-\(\)]/g, '');
+              client.sendSms({
+                to: `+1${phone1}`,
+                from: '+19259058241',
+                // eslint-disable-next-line
+                body: `Request for '${itemName}' is completed!\nContact the other party at: ${phone2} to arrange a time to meet`,
+              }, (error, receiptMessage) => {
+                if (!error) {
+                  // eslint-disable-next-line no-console
+                  console.log('Message sent on: ${receiptMessage.dateCreated}');
+                } else {
+                  // eslint-disable-next-line no-console
+                  console.log('Oops! There was an error.');
+                }
+              });
+              client.sendSms({
+                to: `+1${phone2}`,
+                from: '+19259058241',
+                // eslint-disable-next-line
+                body: `Request for '${itemName} is completed!\nContact the other party at: ${phone1} to arrange a time to meet`,
+              }, (error, receiptMessage) => {
+                if (!error) {
+                  // eslint-disable-next-line no-console
+                  console.log('Message sent on: ${receiptMessage.dateCreated}');
+                } else {
+                  // eslint-disable-next-line no-console
+                  console.log('Oops! There was an error.');
+                }
+              });
+            });
+          });
+    } else {
+      db.User.find({
+        where: {
+          id: req.body.recipientId,
+        },
+      }).then(response => {
+        // eslint-disable-next-line
+        const recipientPhoneNumber = response.dataValues.phone.replace(/[-\(\)]/g, '');
+        console.log(recipientPhoneNumber, '######');
+        client.sendSms({
+          to: `+1${recipientPhoneNumber}`,
+          from: '+19259058241',
+          // eslint-disable-next-line
+          body: `${req.body.text}\nPlease check your notifications at www.sharecrow.com to respond.`,
+        }, (error, receiptMessage) => {
+          if (!error) {
+            // eslint-disable-next-line no-console
+            console.log('Message sent on: ${receiptMessage.dateCreated}');
+          } else {
+            // eslint-disable-next-line no-console
+            console.log('Oops! There was an error.');
+          }
+        });
       });
-      res.sendStatus(200);
-    });
+    }
+    res.sendStatus(200);
   },
-
 };
